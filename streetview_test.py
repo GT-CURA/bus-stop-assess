09d6c7cd7c7a16a197603c73dfa546f4ce796106
brycetjones <brycetjones@gmail.com>
2024-09-17 16:28:00 -0400

    Fixed path issue

--- streetview_test.py
+++ streetview_test.py
@@ -28,22 +28,24 @@ def pull_image(stops, folder_name):
                     'return_error_code': True}
             
             # Try to fetch pic from API 
-            # try: 
-            #     response = requests.get(pic_base, params=pic_params)
-            # except requests.exceptions.RequestException as e: 
-            #     print(f"Failed to pull {row['Record_ID']}: {e}")
-            #     continue 
+            try: 
+                response = requests.get(pic_base, params=pic_params)
+            except requests.exceptions.RequestException as e: 
+                print(f"Failed to pull {row['Record_ID']}: {e}")
+                continue 
             
-            # # Write image
+            # Write image
+            if not os.path.exists("temp"):
+                os.makedirs("temp")
             image_path = "temp" + "/" + f"{row['Record_ID']}" + "_" + f"{degree}" + ".jpg"
-            # with open(image_path, "wb") as file:
-            #     file.write(response.content)
+            with open(image_path, "wb") as file:
+                file.write(response.content)
             
             # Add file to list of paths for this bus stop
             image_paths.append(image_path)
 
             # Close response
-            # sresponse.close()
+            response.close()
         
         # Stitch all the images of this bus stop together
         stitch_images(image_paths, folder_name)
@@ -55,15 +57,15 @@ def stitch_images(image_paths, folder_name):
     # Stitch images 
     stitched_image = np.hstack(images)
 
-    # Remove degree from name lol
+    # Remove path and degree from name lol
     name = image_paths[0].replace('_0', '_stitched')
+    name = name.replace('temp/', '')
     
     # Write into correct folder
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
-    path = os.path.join(folder_name, name)
-
-    cv2.imwrite(name, stitched_image)
+    path = os.getcwd() + "/" + folder_name + "/" + name
+    cv2.imwrite(path, stitched_image)
 
 if __name__ == "__main__":
     # Read API key 
@@ -74,5 +76,5 @@ if __name__ == "__main__":
     bus_stops = pd.read_csv('MARTA.csv')
 
     # Pull test images
-    entry = bus_stops[bus_stops["Record_ID"] == 573]
+    entry = bus_stops[bus_stops["Record_ID"] == 1816]
     pull_image(entry, "Test")
