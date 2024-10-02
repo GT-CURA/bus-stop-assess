2066ac9ffd928b89f57bba86f9c63287242f2dcb
brycetjones <brycetjones@gmail.com>
2024-10-01 22:30:07 -0400

    Runs but doesn't work!!

--- pipeline.py
+++ pipeline.py
@@ -1,18 +1,24 @@
-from tools import streetview
+from tools import streetview, coord
 import pandas as pd 
 
 # Read MARTA's inventory of bus stops 
-bus_stops = pd.read_csv("data/MARTA_cleaned.csv")
+bus_stops = pd.read_csv("data/atl/MARTA_cleaned.csv")
 
-# 
-"""
-# Create instance
-instance = streetview("Stop_Lat", "Stop_Lon", "Stop_ID")
+# Before I forget: 
+# pull coord from csv -> use nearby API to find precise coordinates of bus stop (if needed) -> 
+# pull pano coord from metadata (Free!!) -> pull image from google streetview API -> stitch (if needed)
 
-# Test of streetview tools 
-test_stops = bus_stops[bus_stops["Main_Street_or_Station"] == "ALABAMA ST"]
-instance.pull_image(stops=test_stops, folder_name="Alabama")
+# Create test coordinates
+test_coords = coord(33.781825, -84.407667)
+
+# Create new instance of streetview tools
+instance = streetview("test")
+
+# Calculate heading for this bus stop
+heading = instance.get_heading(test_coords)
+
+# Pull image of bus stop 
+instance.pull_image(test_coords, "test", heading=heading)
 
 # Gather all standalone bus stops
 standalone = bus_stops[(bus_stops["Shelter"] == "No") & (bus_stops["Seating"] != "No, there is no seating")]
-"""
--- tools.py
+++ tools.py
@@ -1,19 +1,23 @@
-import googlemaps.client
 import pandas as pd
-import googlemaps
 import requests
 import os
 import cv2
 import numpy as np
 import onnxruntime as ort
+import math
 
+class coord:
+    def __init__(self, lat: float, lon: float):
+        self.lat = lat
+        self.lon = lon
+    
 class streetview:
     # Parameters for all pics 
     pic_size = "500x500"
-    num_pics = 8
     pic_base = 'https://maps.googleapis.com/maps/api/streetview?'
+    metadata_base = 'https://maps.googleapis.com/maps/api/streetview/metadata?'
 
-    def __init__(self, lat_col: str, lon_col: str, id_col: str):
+    def __init__(self, folder_path):
         """
         Args:
             lat_col (string): Name of the column containing latitude coordinates
@@ -21,14 +25,11 @@ class streetview:
             id_col (string): Name of the column containing bus stop IDs """
         # Read API key 
         self.api_key = open("api_key.txt", "r").read()
-        gmaps = googlemaps.Client(key=self.api_key)
 
         # Set up params
-        self.lat_col = lat_col
-        self.lon_col = lon_col
-        self.id_col = id_col
+        self.folder_path = folder_path
 
-    def improve_coordinates(lat, lon, radius=10):
+    def improve_coordinates(self, coords: coord, radius=10):
         """
         Pull Google's coordinates for a bus stop in the event that the provided coordinates suck
         """
@@ -36,8 +37,8 @@ class streetview:
         location_restriction = {
             'circle':{
                 'center':{
-                    "latitude": lat,
-                    'longitude': lon,
+                    "latitude": coords.lat,
+                    'longitude': coords.lon,
                 },
                 'radius': radius
             }
@@ -55,24 +56,85 @@ class streetview:
         try: 
             response = requests.get(self.pic_base, params=params)
         except requests.exceptions.RequestException as e: 
-            print("oops:",e)
+            print(f"Improving coordinates for {coords.lon}, {coords.lat} failed:",e)
 
         # Pull coordinates from response
         response.content["places"]
     
-    def get_pano_coords(lon, lat):
+    def get_pano_coords(self, coords: coord):
         """
-        Extract coordiantes from a pano's metadata
-        Use the coords returned as the new pano coordinates (they will be more precise)
+        Extract coordiantes from a pano's metadata, used to determine heading
         """
         # Params for request
-    
-    def get_pov():
-        """
-        Use pano's coords to determine the necessary camera POV.
-        """
+        params = {
+            'location': f"{coords.lat},{coords.lon}",
+            'key': self.api_key
+        }
 
-    def pull_image(self, stops: pd.DataFrame, folder_name: str):
+        # Send a request, except faulty responses
+        try: 
+            response = requests.get(self.metadata_base, params)
+        except requests.exceptions.RequestException as e: 
+            print(f"Requesting metadata for image at {coords.lat}, {coords.lon} failed:", e)
+        
+        # Fetch the coordinates from the json response and store in a coords class instance
+        pano_location = response.json().get("location")
+        pano_coords = coord(pano_location["lat"], pano_location["lng"])
+        return pano_coords
+    
+    def get_heading(self, coords: coord):
+        """
+        Use pano's coords to determine the necessary camera FOV.
+        """
+        # Determine the panorama's coords 
+        pano_coords = self.get_pano_coords(coords)
+        
+        d_lng = math.radians(coords.lon - pano_coords.lon)
+        lat1 = math.radians(pano_coords.lat)
+        lat2 = math.radians(coords.lat)
+
+        x = math.sin(d_lng) * math.cos(lat2)
+        y = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(d_lng)
+        
+        initial_bearing = math.atan2(x, y)
+        
+        # Convert radians to degrees and normalize to 0-360 degrees
+        initial_bearing = math.degrees(initial_bearing)
+        compass_bearing = (initial_bearing + 360) % 360
+        return compass_bearing
+
+    def pull_image(self, coords: coord, path: str, fov=120, heading=0):
+        """
+        Pull an image from google streetview 
+        """
+        location = f"{coords.lat},{coords.lon}"
+        pic_params = {'key': self.api_key,
+                    'location': location,
+                    'size': self.pic_size,
+                    'fov': fov,
+                    'heading': heading,
+                    'return_error_code': True}
+            
+        # Try to fetch pic from API 
+        try: 
+            response = requests.get(self.pic_base, params=pic_params)
+        except requests.exceptions.RequestException as e: 
+            print(f"Failed to pull image at coords {coords.lat}, {coords.lon}: {e}")
+        
+        # Make folder if it doesn't exist
+        if not os.path.exists(path):
+            os.makedirs(path)
+        
+        # Write this image segment into the temp folder
+        image_path = path + "/" + f"{coords.lat}{coords.lon}"+".jpg"
+        with open(image_path, "wb") as file:
+            file.write(response.content)
+
+        # Close response, return new image's path
+        response.close()
+        return image_path
+
+    def pull_pano(self, coords: coord, num_pics=8):
         """
         Given a dataframe of coordinates, pulls a panorama of each coordinate from Google Streetview. 
         Pulls num_pics many images and stitches them together into a panorama. 
@@ -81,49 +143,17 @@ class streetview:
             stops (dataframe): All coordinates to pull images of. 
             folder_name (string): Name of the output folder
         """
+        image_paths = []
 
-        # Iterate through each row in dataframe
-        for index,row in stops.iterrows():
+        # Capture 'num_pics' many photos
+        for degree in range(0, 360, int(360/num_pics)):
+            # Save image into temp folder, get its path
+            image_paths.append(self.pull_image(coords.lat, coords.lon, "temp", 360/num_pics, degree))
+        
+        # Stitch all the images of this bus stop together
+        self.stitch_images(image_paths)
 
-            # Set location to match current bus stop
-            location = f"{row[self.lat_col]},{row[self.lon_col]}"
-            image_paths = []
-
-            # Capture 'num_pics' many photos
-            for degree in range(0, 360, int(360/self.num_pics)):
-                pic_params = {'key': self.api_key,
-                        'location': location,
-                        'size': self.pic_size,
-                        'fov': 360/self.num_pics,
-                        'heading': degree,
-                        'return_error_code': True}
-                
-                # Try to fetch pic from API 
-                try: 
-                    response = requests.get(self.pic_base, params=pic_params)
-                except requests.exceptions.RequestException as e: 
-                    print(f"Failed to pull {row[self.id_col]}: {e}")
-                    continue 
-                
-                # Make temp folder to store panorama segments
-                if not os.path.exists("temp"):
-                    os.makedirs("temp")
-                
-                # Write this image segment into the temp folder
-                image_path = "temp" + "/" + f"{row[self.id_col]}" + "_" + f"{degree}" + ".jpg"
-                with open(image_path, "wb") as file:
-                    file.write(response.content)
-                
-                # Add file to list of paths for this bus stop
-                image_paths.append(image_path)
-
-                # Close response
-                response.close()
-            
-            # Stitch all the images of this bus stop together
-            self.stitch_images(image_paths, folder_name)
-
-    def stitch_images(self, image_paths, folder_name):
+    def stitch_images(self, image_paths: str):
         # Load images, create stitcher 
         images = [cv2.imread(image_path) for image_path in image_paths]
 
@@ -135,9 +165,9 @@ class streetview:
         name = name.replace('temp/', '')
         
         # Write into correct folder
-        if not os.path.exists(folder_name):
-            os.makedirs(folder_name)
-        path = os.getcwd() + "/" + folder_name + "/" + name
+        if not os.path.exists(self.folder_path):
+            os.makedirs(self.folder_path)
+        path = os.getcwd() + "/" + self.folder_path + "/" + name
         cv2.imwrite(path, stitched_image)
         
 class yolo:
